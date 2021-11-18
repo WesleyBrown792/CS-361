6f837e93522b213eb246fac41991456bd817144c
Wesley Brown <wesleybrown792@u.boisestate.edu>
2021-11-17 17:32:26 -0700

    Update NFA.java

--- P2/fa/nfa/NFA.java
+++ P2/fa/nfa/NFA.java
@@ -119,6 +119,8 @@ public class NFA implements NFAInterface {
         }
 
         if(to == null||from == null){
+            if(to == null)
+                System.out.println("to is null");
             System.out.println("to or from is still null");
             System.exit(2);
         }
